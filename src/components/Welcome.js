44cef16537facca13d736dde0e76aca8b6982e7c
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-10 15:05:34 +0530

    props

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import React from "react";
+const Welcome = (props) => {
+  return (
+    <>
+      <h1>Hey ! {props.name}</h1>
+      <h2>Welcome to Newton School.</h2>
+    </>
+  );
+};
+
+export default Welcome;
