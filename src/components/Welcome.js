5f582a729fa45f62831e5c5d7042f31e7fe5d9c0
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-10 18:35:14 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import Child from "./Child";
 const Welcome = (props) => {
   return (
     <>
-      <h1>Hey {props.name} !</h1>
+      <h1>Hey {props.name}!</h1>
       <section>
        <h2>Welcome to Newton School.</h2>
       </section>
